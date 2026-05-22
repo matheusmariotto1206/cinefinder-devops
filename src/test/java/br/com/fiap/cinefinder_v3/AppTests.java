2345e4cacd85bb2d9a1e06dbacedee32201895aa
matheusmariotto1206 <matheus_mariotto@hotmail.com>
2026-05-22 20:29:00 -0300

    fix: CI ignora variaveis Oracle da pipeline

--- src/test/java/br/com/fiap/cinefinder_v3/AppTests.java
+++ src/test/java/br/com/fiap/cinefinder_v3/AppTests.java
@@ -3,7 +3,7 @@ package br.com.fiap.cinefinder_v3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=dev")
 class AppTests {
 
     @Test
